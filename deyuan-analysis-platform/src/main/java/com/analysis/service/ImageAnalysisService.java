7487df30bd1f03c1c8125c532b8f826f28865f68
lifenghe-code <li_fenghe@outlook.com>
2025-06-08 16:13:21 +0800

    模拟图像分析的过程

--- deyuan-analysis-platform/src/main/java/com/analysis/service/ImageAnalysisService.java
+++ deyuan-analysis-platform/src/main/java/com/analysis/service/ImageAnalysisService.java
@@ -14,7 +14,7 @@ public class ImageAnalysisService {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            log.info("当前线程：" + Thread.currentThread().getName() + "，第" + count + "次处理完成");
+            log.info("当前线程：" + Thread.currentThread().getName() + "，共第" + count + "次处理完成");
             count.incrementAndGet();
             // 发现违规情况后，调用接口进行行为记录
             // String url = "127.0.0.1:8101/api/detect/violation";
